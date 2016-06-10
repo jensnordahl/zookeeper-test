016ac66dee513a1cf350ba195d6baa56e2ac05df
Jens Nordahl <jens.nordahl@jayway.com>
2016-06-10 14:40:58 +0200

    Register membership by clientName

--- test-client/src/main/java/com/jayway/jno/zookeeper/MembershipDemo.java
+++ test-client/src/main/java/com/jayway/jno/zookeeper/MembershipDemo.java
@@ -2,17 +2,27 @@ package com.jayway.jno.zookeeper;
 
 import java.io.IOException;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.Watcher.Event.EventType;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MembershipDemo {
 	
+    private static final Logger logger = LoggerFactory.getLogger(MembershipDemo.class);
 	private static final int ZOOKEEPER_SESSION_TIMEOUT_MILLIS = 2000;
+	private static final String zooKeeperConnectString = "localhost:2181";
 	private ZooKeeper zooKeeper;
-	private String zooKeeperConnectString = "localhost:2181";
+    private final String clientName;
 	
-	public MembershipDemo() {
+	public MembershipDemo(String clientName) {
+	    this.clientName = clientName;
 		try {
 			zooKeeper = new ZooKeeper(zooKeeperConnectString, ZOOKEEPER_SESSION_TIMEOUT_MILLIS, new ZooKeeperWatcher());
 		} catch (IOException e) {
@@ -21,8 +31,16 @@ public class MembershipDemo {
 	}
 
 	public static void main(String[] args) {
-		MembershipDemo application = new MembershipDemo();
-		application.run();
+	    if (args.length == 1) {
+	        String clientName = args[0];
+	        logger.info("Starting client: {}", clientName);
+	        MembershipDemo application = new MembershipDemo(clientName);
+	        application.run();	        
+	    }
+	    else {
+	        System.err.println("Bad number of args, no client name given");
+	        System.exit(1);
+	    }
 	}
 
 	private void run() {
@@ -36,13 +54,26 @@ public class MembershipDemo {
 		}
 	}
 
+    private void registerSelfAsMember() {
+        try {
+            if (zooKeeper.exists("/members", false) == null) {
+                zooKeeper.create("/members", new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);                
+            }
+            zooKeeper.create("/members/" + clientName, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } 
+    }
+    
 	private class ZooKeeperWatcher implements Watcher {
 
 		@Override
 		public void process(WatchedEvent e) {
-			System.out.println("Event: " + e);
-			
+		    logger.debug("ZooKeeperWatcher got event: {}", e);
+		    if (e.getType() == EventType.None && e.getState() == KeeperState.SyncConnected) {
+	            logger.info("Registering self as member");
+		        registerSelfAsMember();
+		    }
 		}
-		
 	}
 }
